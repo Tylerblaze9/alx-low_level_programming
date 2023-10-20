8ceb4b142966e11846b82e853279504e7020d016
Tylerblaze9 <avidimefrank@gmail.com>
2023-10-19 18:18:37 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,36 +1,36 @@
-#include "main.h" 
- 
- /** 
- ** _strspn - Gets the length of a prefix substring 
- ** @s: Pointer to the string 
- ** @accept: Pointer to the substring containing accepted characters 
- ** Descript: This function calculates the length of the maximum initial 
- ** segment of the string s that consists only of bytes from the 
- ** substring accept. It returns the number of bytes in the initial 
- ** segment of s that consist only of bytes from accept. 
- ** Return: The number of bytes in the initial segment of s that consist only 
- ** of bytes from accept 
- **/ 
- 
- unsigned int _strspn(char *s, char *accept) 
- { 
- unsigned int count = 0; 
- int i, j; 
- int found; 
- for (i = 0; s[i] != '\0'; i++) 
- { 
- found = 0; 
- for (j = 0; accept[j] != '\0'; j++) 
- { 
- if (s[i] == accept[j]) 
- { 
- count++; 
- found = 1; 
- break; 
- } 
- } 
- if (found == 0) 
- break; 
- } 
- return (count); 
- }
+#include "main.h"
+
+/**
+ * _strspn - search a string for a set of bytes
+ * @s: char string array
+ * @accept: char array to check bytes with
+ * Return: Number of bytes in the intial segment of `s`
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i;
+	int j;
+	int c;
+
+	i = 0;
+	c = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				c++;
+				break;
+			}
+			j++;
+		}
+		if (accept[j] == '\0')
+			break;
+		i++;
+	}
+	return (c);
+}
